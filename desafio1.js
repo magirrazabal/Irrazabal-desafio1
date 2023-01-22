24860dbed8232d82d70769efb94e57da044583b0
magirrazabal <mag.irrazabal@gmail.com>
2023-01-22 18:36:28 +0100

    primer desafio

---
+++ desafio1.js
@@ -0,0 +1,51 @@
+class ProductManager {
+    constructor() {
+        this.products = []
+    }
+    static getNewId(lastProduct) {
+        if (!lastProduct) {
+            return 1;
+        } else {
+            return lastProduct.id + 1;
+        }
+    }
+    addProduct(title, description, price, thumbnail, code, stock) {
+        let codes = this.products.map (p => p.code)
+        if (codes.includes(code)) {
+            console.log('Este producto ya existe');
+            return;
+        }
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error('Complete todos los campos');
+            return
+        }
+        let lastProduct = this.products[this.products.length - 1]
+        let newId = ProductManager.getNewId(lastProduct);
+        this.products.push({ title: title, description: description, price: price, thumbnail: thumbnail, code: code, stock: stock, id:newId });
+    }
+    getProducts() {
+        return this.products;
+    }
+    getProductById(id) {
+        let product = this.products.find(p => p.id === id);
+        if (product) {
+            return product;
+        }
+        console.error('Not found');
+    }
+    
+}
+
+//testing 
+
+const productManager = new ProductManager();
+let products = productManager.getProducts();
+console.log(products);
+productManager.addProduct('producto prueba', 'Este es un producto prueba', 200, 'sin imagen', 'abc123', 25);
+products = productManager.getProducts();
+console.log(products);
+
+productManager.addProduct('producto prueba', 'Este es un producto prueba', 200, 'sin imagen', 'abc123', 25);
+
+console.log(productManager.getProductById(1));
+console.log(productManager.getProductById(5));
